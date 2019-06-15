d640bc39b72c736c5abe38e545f1b06924b31a04
blubrandi <blubrandi0@gmail.com>
2019-06-14 22:14:02 -0600

    Readded put and delete endpoints to users.  Removed restricted middleware from users endpoint during dev

--- locker_users/users_router.js
+++ locker_users/users_router.js
@@ -19,26 +19,26 @@ router.get('/:id', (req, res) => {
         .catch(err => res.send(err))
 })
 
-// router.put('/:id', restricted, (req, res) => {
-//     const { id } = req.params
-//     const { password, username, studentname, email } = req.body
-//     Users.update(id, { password, username, studentname, email })
-//         .then(user => {
-//             res.json(user)
-//         })
-//         .catch(error => {
-//             res.status(500).json({ error: error, message: 'Cannot update user' })
-//         });
-// })
+router.put('/:id', (req, res) => {
+    const { id } = req.params
+    const { password, username, studentname, email } = req.body
+    Users.update(id, { password, username, studentname, email })
+        .then(user => {
+            res.json(user)
+        })
+        .catch(error => {
+            res.status(500).json({ error: error, message: 'Cannot update user' })
+        });
+})
 
-// router.delete('/:id', restricted, (req, res) => {
-//     Users.remove(req.params.id)
-//         .then(user => {
-//             res.status(200).json({ message: 'User has been deleted' })
-//         })
-//         .catch(error => {
-//             res.status(500).json({ error, message: 'Error deleting user.' })
-//         })
-// })
+router.delete('/:id', (req, res) => {
+    Users.remove(req.params.id)
+        .then(user => {
+            res.status(200).json({ message: 'User has been deleted' })
+        })
+        .catch(error => {
+            res.status(500).json({ error, message: 'Error deleting user.' })
+        })
+})
 
 module.exports = router
