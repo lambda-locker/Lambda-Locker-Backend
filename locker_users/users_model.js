43ca224c49e540f010502281a4f2f6b320b95d8b
chrisipanaque <chris.ipanaque@gmail.com>
2019-06-14 19:57:57 -0700

    links and notes in students array

--- locker_users/users_model.js
+++ locker_users/users_model.js
@@ -1,62 +1,127 @@
 const db = require('../data/dbConfig.js');
 
 module.exports = {
-    add,
-    update,
-    remove,
-    find,
-    findBy,
-    findById
+  add,
+  update,
+  remove,
+  find,
+  findBy,
+  findById
 };
 
-function find() {
-    const getStudentProfile = async studentId => {
-        const locker_notes = await database('locker_notes')
-            .where({ id: studentId })
-            .first()
+async function find() {
+  // for (i = 0; i < users.length; i++) {
+  //             users[i].notes = await notesById(users[i].business_requesting)
+  //             users[i].volunteer_assigned = await Users.findVolunteerByID(users[i].volunteer_assigned)
+  //             requests.push(r)
+  //         }
+  const users = await db('users');
 
-        const locker_links = await database('locker_links')
-            .where({ id: studentId })
-            .first()
+  for (let user of users) {
+    user.resources = {
+      notes: await db('locker_notes').where({ note_author: user.id }),
+      links: await db('locker_links').where({ link_curator: user.id })
+    };
+  }
 
-        return {
-            student: {
-                id: studentId,
-                locker_notes,
-                locker_links
-            }
-        }
-    }
-
+  return users;
 }
-
-
 
 function findBy(filter) {
-    return db('users').where(filter);
+  return db('users').where(filter);
 }
 
-function findById(id) {
+async function findById(id) {
+  const user = await db('users')
+    .where({ id })
+    .first();
 
-    return db('users')
-        .where({ id })
-        .first();
+  const notes = await db('locker_notes').where({ note_author: id });
+
+  const links = await db('locker_links').where({ link_curator: id });
+
+  return {
+    ...user,
+    notes,
+    links
+  };
 }
 
 async function add(user) {
-    const [id] = await db('users').insert(user);
+  const [id] = await db('users').insert(user);
 
-    return findById(id);
+  return findById(id);
 }
 
 function update(id, changes) {
-    return db('users')
-        .where({ id })
-        .update(changes);
+  return db('users')
+    .where({ id })
+    .update(changes);
 }
 
 function remove(id) {
-    return db('users')
-        .where('id', id)
-        .del();
+  return db('users')
+    .where('id', id)
+    .del();
 }
+
+// const getStudentProfile = async studentId => {
+//     const locker_notes = await database('locker_notes')
+//         .where({ id: studentId })
+//         .first()
+
+//     const locker_links = await database('locker_links')
+//         .where({ id: studentId })
+//         .first()
+
+//     return {
+//         student: {
+//             id: studentId,
+//             locker_notes,
+//             locker_links
+//         }
+//     }
+// }
+
+// router.get('/', restricted, async (req, res) => {
+//     const requests = []
+
+//     try {
+//         const users = await db(users)
+//         for (i = 0; i < users.length; i++) {
+//             users[i].notes = await notesById(users[i].business_requesting)
+//             users[i].volunteer_assigned = await Users.findVolunteerByID(users[i].volunteer_assigned)
+//             requests.push(r)
+//         }
+//         res.status(200).json(requests)
+//     } catch (err) {
+//         res.send(err)
+//     }
+// })
+
+// const users = await db('users')
+// .join('locker_links', 'users.id', 'locker_links.link_curator')
+// .innerJoin('locker_notes', 'users.id', 'locker_notes.note_author')
+// .select(
+//   'users.student_name',
+//   'users.email',
+//   'users.cohort',
+//   dbAccess.raw('COUNT(locker_notes.id) as notes')
+// )
+
+// .join('locker_notes as notes', 'notes.note_author', 'users.id')
+// .join('locker_links as links', 'links.link_curator', 'users.id')
+// .select(
+//   'users.student_name',
+//   'users.email',
+//   'users.cohort',
+//     db.raw('COUNT(notes.id) as notesPosted'),
+//   db.raw('COUNT(links.id) as linksPosted')
+// )
+// .groupBy('users.id');
+
+// .count('locker_links.id as links')
+// .count('locker_notes.id as notes')
+// .groupBy('users.id');
+
+//   return users;
